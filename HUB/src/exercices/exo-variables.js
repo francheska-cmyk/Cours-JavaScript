6d461bfeff9d250e2b86175cf3ab8dba3a28f17b
francheska-cmyk <charlotfrancheska@gmail.com>
2026-05-11 14:36:18 +0200

    Création fichier

---
+++ HUB/src/exercices/exo-variables.js
@@ -0,0 +1,24 @@
+let string="francheska";
+console.log(string);
+let number="21";
+console.log(number);
+let decimal="15.9"
+console.log(decimal)
+let array=["bonjour","francheska",28];
+console.log(array);
+console.log(array[2]);
+console.log(array.length);
+
+let object ={
+    message: "bonjour",
+    prenom: "francheska",
+    age: "28"
+};
+
+console.log(object);
+console.log(object.message);
+
+function salutation() {return "Hello world";}
+let meeting = salutation();
+console.log(meeting); 
+
