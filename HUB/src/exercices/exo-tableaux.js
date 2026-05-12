67dc62c36682da1d55511223f1a882c874cb7c69
francheska-cmyk <charlotfrancheska@gmail.com>
2026-05-12 14:09:47 +0200

    ajout exo sur les tableaux

---
+++ HUB/src/exercices/exo-tableaux.js
@@ -0,0 +1,10 @@
+let nom ="José"
+let age = "42 ans"
+let passions= ["badminton", "danse"];
+let tabUser = [
+    (nom), (age),(passions)
+];
+
+console.log(tabUser);
+console.log(tabUser[2]);
+console.log(tabUser[2][1]);
